fb93f3101fa04005b64a3bfee5ed6909b3a2733f
Magdalena Kaprińska <159182249+magdalena-karpinska@users.noreply.github.com>
2024-05-19 13:49:27 +0200

    Adds comments

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+// Step 1: Import the build-in 'http' module on Node.js, which allows to create an HTTP server.
 const http = require('http');
 
+// Step 2: Create the server
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello fellow developer!');
+    res.statusCode = 200; // Sets status code to 200 (OK)
+    res.setHeader('Content-Type', 'text/plain'); // Set content type to plain text
+    res.end('Hello fellow developer!'); // Send response and end the connection
 });
 
-const hostname = 'localhost';
-const port = 3000;
+// Step 3: Define the hostname and port
+const hostname = 'localhost'; // Hostname set to 'localhost'
+const port = 3000; // Port set to 3000
 
+// Step 4: Start the server
 server.listen(port, hostname, () => {
     console.log(`Server running at http;//${hostname}:${port}/`)
 });
