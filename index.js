7bcd69eef56cebe43d3a919c93de132de53349ee
Magdalena Kaprińska <159182249+magdalena-karpinska@users.noreply.github.com>
2024-05-19 19:23:58 +0200

    Adds Morgan-logging middleware

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+// Import Express
 const express = require('express');
+const morgan = require('morgan');
+
+// Create an app
 const app = express();
 
+app.use(morgan('tiny'));
+// Serve static files
+app.use(express.static('static'));
+
+//Define a route
 app.get('/', (req, res) => {
     res.send('Hello fellow developer');
 });
 
+//Set the port
 const port = 3000;
+
+// Start the server
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
 });
