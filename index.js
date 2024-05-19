4d2c75f091951dfbcc28110622d2b309de5aac43
Magdalena Kaprińska <159182249+magdalena-karpinska@users.noreply.github.com>
2024-05-19 18:20:11 +0200

    Adds 404.html file and fixes bugs

--- index.js
+++ index.js
@@ -21,35 +21,49 @@
 
 // Function to get the file name from URL
 const fileNameOfUrl = (url) => {
-    let fileName = '';
-    // Check if the URL path is just the root
-    if(url.split('/')[1] === '') {
-        fileName = 'index.html'; // Default to index.html for the root URL
-    } else {
-        fileName = url.split('/')[1]; // Get the second part of the URL path
-    }
+    let fileName = url.split('/')[1] || 'index.html';
     return fileName;
+
+};
+// Function to get file content or serve a 404 page
+const getFileContentOr404 = (fileName) => {
+    const filePath = `./static/${fileName}`;
+    if(!fs.existsSync(filePath)) {
+        fileName = '404.html';
+    }
+    return fs.readFileSync(filePath, 'utf-8');
 };
 
+// Server setup
 const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(req);
     console.log(`The URL for the request was ${req.url}`);
     console.log(`The Method for the request was ${req.method}`);
    
     const fileName = fileNameOfUrl(req.url);
-    const content = fs.readFileSync(`./static/${fileName}`, 'utf-8');
+
+    // Check for favicon.ico request and handle it separately
+    if(fileName === 'favicon.ico') {
+        res.statusCode = 404;
+        res.end('');
+        return;
+    }
+
+    // Get file content or serve a 404 page if the file doesn't exist
+    const content = getFileContentOr404(fileName);
 
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end(content);
 });
 
+// Server configuration
 const hostname = 'localhost';
 const port = 3000;
 
+// Start the server
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
