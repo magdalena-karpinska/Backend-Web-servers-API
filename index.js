b24f1fbdbaa1e818fd954ed079d2cf0eea12c822
Magdalena Kaprińska <159182249+magdalena-karpinska@users.noreply.github.com>
2024-05-20 23:13:29 +0200

    Adds a POST route

--- index.js
+++ index.js
@@ -15,9 +15,11 @@ const db = [
     {
         id: 3,
         name: 'Uttejh Reddy',
-        email: 'uttejhreddy.com'
+        email: 'uttejhreddy@gmail.com'
     }
 ];
+
+app.use(express.json());
 
 app.get('/api/developers/:id', (req, res) => {
     const dev = db.find(dev => dev.id == req.params.id); //'==' allows for a comparison between a string (route params) and a numbern (id)
@@ -26,6 +28,20 @@ app.get('/api/developers/:id', (req, res) => {
         return;
     }
     res.json(dev);
+});
+
+app.post('/api/developers/', (req, res) => {
+    const newDeveloper = {
+        id: db.length + 1,
+        name: req.body.name,
+        email: req.body.email,
+    };
+    db.push(newDeveloper);
+
+    res
+        .status(201)
+        .setHeader('location', `api/developers/${newDeveloper.id}`)
+        .json(newDeveloper);
 });
 
 const port = 3000;
